d0ad2acdb98c0a5a037770cf52f6c199f1ace2ee
Félix L. Morales <12535179+morales-felix@users.noreply.github.com>
2024-06-15 22:34:56 -0500

    Took away Windows-specific libraries; Updated redirect URI

--- ehr_launch.py
+++ ehr_launch.py
@@ -17,7 +17,7 @@ BASE_URL = "https://launch.smarthealthit.org/v/r4/fhir"
 SCOPES = (
     "patient/Patient.rs patient/Observation.rs launch offline_access openid fhirUser"
 )
-REDIRECT_URI = "http://localhost:4201/fhir-app/"
+REDIRECT_URI = "https://smart-on-fhir-python-app.onrender.com/fhir-app/"
 
 
 ####################################################################################################
